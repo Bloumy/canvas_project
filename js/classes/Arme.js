181d591742f552696a1d78bee4d27647d0da4aea
ahorde <alexis.horde@ign.fr>
2016-03-02 18:22:55 +0100

    Sauvegarde 2

---
+++ js/classes/Arme.js
@@ -0,0 +1,18 @@
+function Commandes(){
+    
+}
+
+Commandes.prototype.DIRECTION = {
+    "HAUT": 0,
+    "HAUTDROITE":1,
+    "DROITE": 2,
+    "BASDROITE":3,
+    "BAS": 4,
+    "BASGAUCHE":5,
+    "GAUCHE": 6,
+    "HAUTGAUCHE":7
+};
+
+
+module.exports = Commandes;
+
